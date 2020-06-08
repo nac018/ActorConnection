b9aef744c2bf72e9b7428d6e240a78614689897d
Tommy Chen <32497892+nac018@users.noreply.github.com>
2020-06-08 11:24:00 -0700

    Update ActorGraph.cpp

--- ActorGraph.cpp
+++ ActorGraph.cpp
@@ -1,13 +1,3 @@
-/*
- * ActorGraph.cpp
- * Author: Nang Chen (cs100fao) , Haonan Huang (cs100fbd)
- * Date:   12/01/2017
- *
- * This file is meant to exist as a container for starter code that 
- * you can use to read the input file format defined in movie_casts.tsv. 
- * Feel free to modify any/all aspects as you wish.
- */
-
 #include <fstream>
 #include <iostream>
 #include <sstream>
